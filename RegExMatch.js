ad0a06340004e16c009eae9f686e04ceb9adb8e8
Micha Mazaheri <micha@mazaheri.me>
2016-05-05 14:24:47 +0900

    Fix display of dynamic value

--- RegExMatch.js
+++ RegExMatch.js
@@ -1,13 +1,11 @@
 var RegExMatch = function() {
 	this.evaluate = function() {
-		
-		var flags = 'g';
 
+		var flags = 'g';
 		flags += this.ignoreCase ? 'i' : '';
 		flags += this.multiline ? 'm' : '';
 
 		var regex = new RegExp(this.re, flags);
-
 
 		var m = regex.exec(this.input);
 		if(m != null) {
@@ -23,23 +21,22 @@ var RegExMatch = function() {
 		}
 	}
 
-
 	this.text = function(context) {
-		return " ➤ " + this.input;
+		if (!this.input && this.input.length === 0) {
+			return null;
+		}
+		return "➤ " + this.input;
 	}
 }
 
 RegExMatch.identifier = "com.luckymarmot.RegExMatch";
-
-RegExMatch.title = "RegEx Match";
-
+RegExMatch.title = "RegExp Match";
 RegExMatch.help = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp#Special_characters_meaning_in_regular_expressions";
-
 RegExMatch.inputs = [
-	InputField("re", "RegEx", "String"),
+	InputField("re", "RegExp", "String"),
 	InputField("input", "Input", "String"),
-	InputField("ignoreCase","ignore case", "Checkbox", {defaultValue: false}),
-	InputField("multiline","multiline", "Checkbox", {defaultValue: false})
+	InputField("ignoreCase","Case-insensitive", "Checkbox", {defaultValue: false}),
+	InputField("multiline","Multiline", "Checkbox", {defaultValue: false})
 ]
 
 registerDynamicValueClass(RegExMatch);
